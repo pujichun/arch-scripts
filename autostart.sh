e11adf377186f5381a2ba11e3664113fdd44c436
pujichun <pujichun@outlook.com>
2022-01-12 23:02:20 +0800

    first commit

---
+++ autostart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+$HOME/scripts/wallpapers.sh &
+$HOME/scripts/dwm-status.sh &
+
+blueman-applet &
+$HOME/scripts/fcitx5.sh &
---
+++ vol-down.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+/usr/bin/amixer -c 0 -q set Master 5%- unmute
+
+bash ~/scripts/dwm-status-refresh.sh
